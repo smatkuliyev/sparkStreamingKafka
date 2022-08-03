451f7679ac064ed281a486d02da72d4ea6a7a0f6
Sulayman Matkuliyev <suleymantm11@gmail.com>
2022-08-03 12:10:36 +0300

    v1

---
+++ src/main/java/SearchProductModel.java
@@ -0,0 +1,28 @@
+public class SearchProductModel {
+    private String product;
+    private String time;
+
+    public SearchProductModel() {
+    }
+
+    public SearchProductModel(String product, String time) {
+        this.product = product;
+        this.time = time;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
